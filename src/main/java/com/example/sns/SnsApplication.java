ed028720de0b4731602d1c2cf4e9da4aef6cf80b
junghwan <jhzzang97@gmail.com>
2024-01-09 14:07:36 +0900

    자바 및 스프링 버전 변경

--- src/main/java/com/example/sns/SnsApplication.java
+++ src/main/java/com/example/sns/SnsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) // 자동으로 데이터베이스 config하는 것을 방지
 public class SnsApplication {
 
 	public static void main(String[] args) {
